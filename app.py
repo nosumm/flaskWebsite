573bbf2bc8e4ba47d0a99800d87d3de81ce8d5ee
Noah Staveley <noahstaveley@gmail.com>
2024-12-01 19:25:48 -0800

    updates

--- app.py
+++ app.py
@@ -10,7 +10,22 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/project1')
+def project1():
+    return render_template('project1.html')
+
+@app.route('/project2')
+def project2():
+    return render_template('project2.html')
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+if __name__ == '__main__':
+    app.run(debug=True)
